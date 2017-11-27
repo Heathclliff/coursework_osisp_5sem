96d858fe07aaebe5448e4a7779100e81ee83cf85
Heathclliff <potapchik97@mail.ru>
2017-11-28 00:49:36 +0300

    added some managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	#region Variables
+
+	public static GameManager instanse;
+
+	[SerializeField] GameObject prefabManager;
+
+	#endregion
+
+	#region Events
+
+	public static System.Action OnGameLoad;
+	public static System.Action OnGameStart;
+	public static System.Action OnLevelEnd;
+	public static System.Action OnGameRestart;
+
+
+	#endregion
+
+
+	#region Unity lifecycle
+
+	void Awake()
+	{
+		if (instanse == null) 
+		{
+			instanse = this;
+		} 
+		else 
+			if(instanse != this)
+			{
+				Destroy (gameObject);
+			}
+	}
+
+
+	void Start()
+	{
+		GameObject prefab = Instantiate (prefabManager);
+		prefab.transform.parent = this.gameObject.transform;
+	}
+
+	#endregion
+}
